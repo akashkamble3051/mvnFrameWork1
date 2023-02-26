bf72df6cc31718a97e329b549909703120ce3c97
akashkamble <akashkamble3051@gmail.com>
2023-02-26 12:36:53 +0530

    4th commit

--- MyBDDCucumberFramework/src/test/java/runner/TestRunner.java
+++ MyBDDCucumberFramework/src/test/java/runner/TestRunner.java
@@ -2,7 +2,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//cucumberProject
 @CucumberOptions(features = { "src/test/resources/features/VTigerCompaign.feature" }, glue = {
 		"stepDefinition" }, plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"rerun:target/failedScenario.txt"},tags="smoke")
